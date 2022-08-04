c4b378285849e7be01e7a64a2c976a8c049ee12a
Diego Lima Araújo <70676146+diegolimaaraujo@users.noreply.github.com>
2022-08-04 18:15:57 -0300

    Create aula3.ex

---
+++ aula3.ex
@@ -0,0 +1,3 @@
+console.log('Meu nome é "Diego Lima". Estou aprendendo JavaScript às', 6,'da tarde.'); // método log dentro do objeto console
+// apresenta uso de strings na mesma linha da variável number
+// relaciona aspas diferentes conforme necessidade da string
